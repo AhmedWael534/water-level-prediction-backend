649d55a72d5f8bd05e3d5ae16c773f64e0f85222
AhmedWael534 <awael534@gmail.com>
2024-06-12 15:29:41 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import os
 import joblib
 from flask import Flask, request, jsonify
@@ -57,3 +63,10 @@ def predict():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+# In[ ]:
+
+
+
+
